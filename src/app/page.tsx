64734f9f0c33c1d8dad82691212d84aefb98b62c
Justin Davenport <justindavenport.space@gmail.com>
2024-06-07 12:44:20 -0400

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,12 +65,12 @@ export default function Home() {
 
   return (
     <main>
-      <div className="logo">
+      {/* <div className="logo">
         <span style={{ fontWeight: 600 }}>Justin Davenport</span>
         <p style={{ fontSize: "clamp(12px, 2vw, 16px)" }}>
           Web Developer&ensp;|&ensp;Product Designer
         </p>
-      </div>
+      </div> */}
 
       <div className="background"></div>
       <div className="overlay"></div>
@@ -107,17 +107,20 @@ export default function Home() {
           >
             <div className="text-holder">
               <div className="splash-title-holder">
-                <h1 className="splash-title-right">Web Developer</h1>
+                {/* <h1 className="splash-title-right">Web Developer</h1> */}
+                <h1 className="splash-title-right">WEB DEVELOPER</h1>
                 <h1 className="divider">
                   <b style={{ fontWeight: "200", lineHeight: "1" }}>|</b>
                 </h1>
-                <h1 className="splash-title-left">Product Designer</h1>
+                {/* <h1 className="splash-title-left">Product Designer</h1> */}
+                <h1 className="splash-title-left">PRODUCT DESIGNER</h1>
               </div>
               <p className="splash-p">
                 I&apos;m Justin, a Web Developer and Product Designer located in
-                Charlotte, NC. <br className="divider" />
+                Charlotte, NC.
+                {/* <br className="divider" />
                 My expertise lies in crafting dynamic web experiences using
-                HTML, CSS, and JavaScript primarily within Next.js.
+                HTML, CSS, and JavaScript primarily within Next.js. */}
               </p>
               <div
                 style={{
@@ -223,6 +226,16 @@ export default function Home() {
                   </Link>
                 </div>
               </div>
+
+              {/* <svg
+                className="pulse"
+                xmlns="http://www.w3.org/2000/svg"
+                height="28"
+                width="24"
+                viewBox="0 0 512 512"
+              >
+                <path d="M256 464a208 208 0 1 1 0-416 208 208 0 1 1 0 416zM256 0a256 256 0 1 0 0 512A256 256 0 1 0 256 0zM376.9 294.6c4.5-4.2 7.1-10.1 7.1-16.3c0-12.3-10-22.3-22.3-22.3H304V160c0-17.7-14.3-32-32-32l-32 0c-17.7 0-32 14.3-32 32v96H150.3C138 256 128 266 128 278.3c0 6.2 2.6 12.1 7.1 16.3l107.1 99.9c3.8 3.5 8.7 5.5 13.8 5.5s10.1-2 13.8-5.5l107.1-99.9z" />
+              </svg> */}
             </div>
           </div>
 
