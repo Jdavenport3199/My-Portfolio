45f4d2b7d863b718766b4948dc86eed37e56161a
Justin Davenport <justindavenport.space@gmail.com>
2024-07-24 19:58:29 -0400

    +

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
           <div
             className="container-holder"
             style={{
-              minHeight: "100vh",
+              minHeight: "95vh",
               display: translateX === "-50%" ? "flex" : "none",
               alignItems: "flex-end",
             }}
@@ -110,17 +110,23 @@ export default function Home() {
               <h2
                 style={{
                   fontSize: "clamp(16px, 4vw, 24px)",
-                  marginTop: "1rem",
+                  marginTop: "0.8rem",
                 }}
               >
-                Located in Charlotte,
-                <br className="break" />
-                {" "}available for global opportunities.
+                Located in Charlotte, available globally.
               </h2>
             </div>
             <div className="footer-holder" id="fade">
               <div className="footer">
-                <p>Explore my work below, or contact me for collaborations.</p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="20"
+                  width="20"
+                  viewBox="0 0 512 512"
+                  id="rotate"
+                >
+                  <path d="M418.4 157.9c35.3-8.3 61.6-40 61.6-77.9c0-44.2-35.8-80-80-80c-43.4 0-78.7 34.5-80 77.5L136.2 151.1C121.7 136.8 101.9 128 80 128c-44.2 0-80 35.8-80 80s35.8 80 80 80c12.2 0 23.8-2.7 34.1-7.6L259.7 407.8c-2.4 7.6-3.7 15.8-3.7 24.2c0 44.2 35.8 80 80 80s80-35.8 80-80c0-27.7-14-52.1-35.4-66.4l37.8-207.7zM156.3 232.2c2.2-6.9 3.5-14.2 3.7-21.7l183.8-73.5c3.6 3.5 7.4 6.7 11.6 9.5L317.6 354.1c-5.5 1.3-10.8 3.1-15.8 5.5L156.3 232.2z" />
+                </svg>
                 <div className="social-btn-holder">
                   <Link
                     href="https://www.linkedin.com/in/justindavenport99/"
