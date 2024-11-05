c429605eeff60c99cdaf3a6b2e112b7ef80e67c5
Justin Davenport <justindavenport.space@gmail.com>
2024-11-05 14:03:03 -0500

    +

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -6,7 +6,7 @@ const Background = ({}: Props) => {
   return (
     <>
       <video className="video" id="fade" autoPlay loop muted playsInline>
-        <source src="/Lines.mp4" type="video/mp4" />
+        <source src="/background.mp4" type="video/mp4" />
       </video>
     </>
   );
